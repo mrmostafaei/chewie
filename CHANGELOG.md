6afed2eb0cf6a606ad2b733a88bf28aad8794d08
Milad Mostafaei <mr.mostafaeei@gmail.com>
2024-10-20 06:45:29 +0330

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## [1.8.5]
+* Adding Seek buttons for Android.
+* Adding Video Player Cache.
+* Upgraded `wakelock_plus` to version `1.2.8`, which uses `web` version `1.0.0`.
---
+++ lib/src/cupertino/cupertino_progress_bar.dart
@@ -0,0 +1,40 @@
+import 'package:cached_video_player_plus/cached_video_player_plus.dart';
+import 'package:chewie/src/chewie_progress_colors.dart';
+import 'package:chewie/src/progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:video_player/video_player.dart';
+
+class CupertinoVideoProgressBar extends StatelessWidget {
+  CupertinoVideoProgressBar(
+    this.controller, {
+    ChewieProgressColors? colors,
+    this.onDragEnd,
+    this.onDragStart,
+    this.onDragUpdate,
+    super.key,
+    this.draggableProgressBar = true,
+  }) : colors = colors ?? ChewieProgressColors();
+
+  final CachedVideoPlayerPlusController controller;
+  final ChewieProgressColors colors;
+  final Function()? onDragStart;
+  final Function()? onDragEnd;
+  final Function()? onDragUpdate;
+  final bool draggableProgressBar;
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoProgressBar(
+      controller,
+      barHeight: 5,
+      handleHeight: 6,
+      drawShadow: true,
+      colors: colors,
+      onDragEnd: onDragEnd,
+      onDragStart: onDragStart,
+      onDragUpdate: onDragUpdate,
+      draggableProgressBar: draggableProgressBar,
+    );
+  }
+}
